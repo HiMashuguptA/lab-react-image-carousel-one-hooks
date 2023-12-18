15ece48aa520aa3acf9ae4c092fd0349dd46d317
HiMashuguptA <sg707012@gmail.com>
2023-12-19 01:51:23 +0530

    Final

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
 import "./Carousel.css";
-import { images } from "../data/CarouselData";
+import { Image } from "@mui/icons-material";
 
 // you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
-function Carousel() {
+function Carousel(props) {
+    var[val,setvalue]=useState(0);
+    const handleClick=()=>{
+        if(val<props.data.length-1){
+            setvalue(val+1)
+            console.log(setvalue);
+        }
+    }
+    const handleClick1=()=>{
+        if(val>0){
+            setvalue(val-1)
+        }
+    }
+    return(
+        <>
+        <h1>Carousel</h1>
+        <div className="carousel-container flex">
+            <div className="leftArrow arrowDiv flex" onClick={handleClick1} >
+                <ArrowBackIosIcon/>
+            </div>
+            <h2 className="title">{props.data[val].title}</h2>
+            <img src={props.data[val].img} alt="" />
+            <h4 className="caption">{props.data[val].subtitle}</h4>
+            <div className="rightArrow arrowDiv flex" onClick={handleClick}>
+                <ArrowForwardIosIcon/>
+            </div>
+
+        </div>
+    </>
+    )
 }
 
 export default Carousel;
--- src/data/CarouselData.jsx
+++ src/data/CarouselData.jsx
@@ -2,7 +2,7 @@ import faces from "../images/faces.jpg";
 import girlface from "../images/girlface.jpg";
 import eyes from "../images/eyes.jpg";
 
-export const images = [
+const images = [
   { title: "Faces", subtitle: "How many faces do you have?", img: faces },
   {
     title: "Inside out",
@@ -15,3 +15,4 @@ export const images = [
     img: eyes,
   },
 ];
+export default images
